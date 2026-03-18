f0f19eb3cd572a32a7926228a549e90d2c3f28d8
sanket255 <sanketwaghmare340@gmail.com>
2026-03-18 19:17:24 +0530

    while loop written with understaing and lookingat some references online of other while loop examples and nested a if/elif ladder too

--- day-2/main.py
+++ day-2/main.py
@@ -9,17 +9,17 @@ guess= 0
 guess_list= []
 guess_taken = 0
 
-# while guess!=numgen:
-    # 
-    # guess=int(input("guess the number from 1-100:" ))
-    # guess_list.append(guess)
-    # guess_taken +=1
-    # if guess==numgen:
-        # print(f'{guess} {"good boiii why dont you try counting card next time"}')
-    # elif  guess>=numgen:
-        # print("too optimistic low the bar a bit:" )
-    # elif guess<=numgen:
-        # print("why so gloomy its not like we are asking to score your appereance here:" )
+while guess!=numgen:
+    
+    guess=int(input("guess the number from 1-100:" ))
+    guess_list.append(guess)
+    guess_taken +=1
+    if guess==numgen:
+        print(f'{guess} {"good boiii why dont you try counting card next time"}')
+    elif  guess>=numgen:
+        print("too optimistic low the bar a bit:" )
+    elif guess<=numgen:
+        print("why so gloomy its not like we are asking to score your appereance here:" )
     # 
 # 
 # print(guess_list,guess_taken)
