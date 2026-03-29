0e7acf22736ec53d9639216a5dee77b958ba0c3f
sanket255 <sanketwaghmare340@gmail.com>
2026-03-30 01:19:24 +0530

    got done with day-4 programme

--- day-4/main.py
+++ day-4/main.py
@@ -1,20 +1,31 @@
 print("password cheacker!!")
 
-
-user_pw=input("enter the password to check it's strength: ")
-pwlen=len(user_pw)
+initi=input("do you wanna check password???: ")
 
 
-if pwlen < 8 :
-    print(f"{pwlen}an 3 year old with basic social engineering can hack your password, level up!!!")
-if user_pw.isdigit() == "true":
-    print("better but still i can look up in your instagram account like its nothin!!")
-if user_pw.isupper() == "true":
-    print("so now you have some decent level of security stil hackable!!!")
-if user_pw.isalpha() == "true":
-    print("superduperunhackable af!!!")
+while initi != "quit":
+    strength=0
+    user_pw=input("enter the password to check it's strength: ")
+    for char in user_pw:
+        if char.isdigit():
+            strength+=1
+            break
+    for char in user_pw:
+        if char.isupper():
+            strength+=1
+            break
+    for char in user_pw:
+        if char.isalpha():
+            strength+=1
+            break
 
+    if strength < 2 :
+        print("an 3 year old with basic social engineering can hack your password, level up!!!")
+    if strength == 2 :
+        print("your password is decent but hackable!!")
+    if strength > 2 :
+        print("fuk bro your password is SuperDuperUnHackable!!!!!")
 
-
+    initi=input("do you wanna do another password check (y/quit): " )
 
 
