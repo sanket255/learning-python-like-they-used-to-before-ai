afcd13c9fd61baa75d09d481269cde615ff3671c
sanket255 <sanketwaghmare340@gmail.com>
2026-04-01 20:24:10 +0530

    built yet another calculator thins time using functions man im loosing track of how many calculators i've built wait i can read https://sanket255.github.io/scar-log/ and count it hahaha

--- day-5/main1.py
+++ day-5/main1.py
@@ -1,2 +1,29 @@
-print("fonctions")
+print("functions")
 
+a=int(input("Enter the number 1 that you want to oparate: "))
+b=int(input("Enter the number 2 that you want to oparate: "))
+c=input("Enter the oparator that you want to oparate (+, -, *, /): ")
+
+def add (a, b, c):
+    if c == "+":
+        print(a+b)
+
+def sub (a, b, c):
+    if c == "-":
+        print (a-b)
+
+def mul (a, b, c):
+    if c == "*":
+        print(a*b)
+
+def div (a, b, c):
+    if c == "/":
+        print(a/b)
+
+def calculator(add, sub, mul, div):
+    add(a, b, c)
+    sub(a, b, c)
+    mul(a, b, c)
+    div(a, b, c)
+
+calculator(add, sub, mul, div)
