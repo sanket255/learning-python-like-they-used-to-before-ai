f3cee7a5552dc2bfe9cc200d581fc70806d89f09
sanket255 <sanketwaghmare340@gmail.com>
2026-04-01 21:05:14 +0530

    i found out that there is a return function too and recreated this function calculator using the return functions man i really dont know much about these things

--- day-6/main.py
+++ day-6/main.py
@@ -4,26 +4,31 @@ a=int(input("Enter the number 1 that you want to oparate: "))
 b=int(input("Enter the number 2 that you want to oparate: "))
 c=input("Enter the oparator that you want to oparate (+, -, *, /): ")
 
-def add (a, b, c):
-    if c == "+":
-        print(a+b)
+def add (a, b):
+    return a + b
 
-def sub (a, b, c):
-    if c == "-":
-        print (a-b)
+def sub (a, b):
+    return a - b
 
-def mul (a, b, c):
-    if c == "*":
-        print(a*b)
+def mul (a, b):
+    return a * b
 
-def div (a, b, c):
-    if c == "/":
-        print(a/b)
+def div (a, b):
+    if b==0:
+        return "Error:cannot divide by 0"
+    else:
+        return a / b
 
-def calculator(add, sub, mul, div):
-    add(a, b, c)
-    sub(a, b, c)
-    mul(a, b, c)
-    div(a, b, c)
+def calculator(a,b,c):
+    
+    if c=="+":
+       return add(a,b)
+    elif c=="-":
+        return sub(a,b)
+    elif c=="*":
+        return mul(a,b)
+    elif c=="/":
+        return div(a,b)
 
-calculator(add, sub, mul, div)
+result= calculator(a,b,c)
+print(f"the result is: {result}")
