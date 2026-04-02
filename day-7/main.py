95e35f45e7cfe4f0e0d39ae42c13e3c9f21a79c5
sanket255 <sanketwaghmare340@gmail.com>
2026-04-02 23:21:26 +0530

    section-1 is getting done

---
+++ day-7/main.py
@@ -0,0 +1,40 @@
+
+print ("function infused file I/O")
+
+
+date=(input("(Insert the date): "))
+# queue =input("enter:")
+
+# note=input("write a note to remind yourself who you were today: ")
+
+# 1. save_note(filename, note) - saves a note to file
+def write():
+    with open ("note.txt", "a") as file :
+        file.write(input("write a note for your futureself: ") + "\n")
+        # file.write( note +"\n")
+
+
+# 2. read_notes(filename) - reads all notes from file
+# def read():
+#     with open ("note.txt", "r") as file:
+#          print(file.read())
+
+
+# 3. Main code - asks user to save or read
+# def main():
+#     while date != "quit":
+        
+#         queue =input("you wanna naot the note: ")
+#         if date == "y":
+#             write()
+            
+#         date = input("do you wanna read the note?")
+#         if date == "y":
+#             read()
+#         date=input("do you wanna save another note or wanna quit (y/quit)")
+        
+
+
+
+
+
