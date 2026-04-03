0317729d074dce1b22cc75f3b0d1190c5f75fdad
sanket255 <sanketwaghmare340@gmail.com>
2026-04-03 21:04:40 +0530

    write the main function theres a delete function concept that will be coded on sunday as its improvement day

--- day-8/main.py
+++ day-8/main.py
@@ -22,13 +22,32 @@ def read():
     with open ("journal.txt", "r") as file:
         print(file.read())
 
+# read()
+
 # # Search entries by keyword
 def search():
     with open ("journal.txt", "r") as file:
-        keyword=input("Search note through journal date/word of your choice")
+        keyword=input("Search note through journal date/word of your choice: ")
         for line in file:
             if keyword in line:
                 print(line)
 
+# search()
+
 # Delete an entry (optional, harder)
 # Loop until quit
+def main():
+    ask=0
+    while ask != "quit":
+        ask=input("what do you want to do write or save(w)  / read(r) / search(s) /delete(del) or quit(quit): ")
+        if ask== "w":
+            write()
+        elif ask=="r":
+            read()
+        elif ask=="s":
+            search()
+        # ask=("Do  you want to enter another note (y) or you want to quit (quit): ")
+        elif ask  == "quit":
+            break
+
+main()
