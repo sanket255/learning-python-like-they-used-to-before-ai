c6e94ac589e3f77fb937f9687892f4e2a45959ab
sanket255 <sanketwaghmare340@gmail.com>
2026-04-05 19:45:04 +0530

    q1 added

---
+++ day-10/main.py
@@ -0,0 +1,78 @@
+# Question: Which planet is closest to the sun?
+
+#     Answer: Mercury.
+
+# Question: How many hearts does an octopus have?
+
+#     Answer: Three.
+
+# Question: What is the only continent without an active volcano?
+
+#     Answer: Australia.
+
+# Question: What is the smallest country in the world?
+
+#     Answer: The Vatican.
+
+# Question: What is the name of the pet dinosaur on "The Flintstones"?
+
+#     Answer: Dino
+
+
+# print("random quizz")
+
+def q1():
+    print("Question: Which planet is closest to the sun?")
+    ans=input("enter you answer: ")
+
+    if ans == "mercury":
+        print("correct")
+    elif ans!="mercury":
+        print("incorrect")
+
+q1()
+
+# def q2():
+#     print("Question: How many hearts does an octopus have?")
+#     ans=input("enter you answer: ")
+
+#     if ans == "three" or ans == "3":
+#         print("correct")
+#     elif ans == "three" or ans == "3":
+#         print("incorrect")
+
+# q2()
+
+# def q3():
+#     print("What is the only continent without an active volcano?")
+#     ans=input("enter you answer: ")
+
+#     if ans == "australia":
+#         print("correct")
+#     elif ans!="australia":
+#         print("incorrect")
+
+# q3()
+
+# def q4():
+#     print("Question: What is the smallest country in the world?")
+#     ans=input("enter you answer: ")
+
+#     if ans == "the vatican":
+#         print("correct")
+#     elif ans!="the vatican":
+#         print("incorrect")
+
+# q4()
+
+# def q5():
+#     print("Question: What is the name of the pet dinosaur on The Flintstones?")
+#     ans=input("enter you answer: ")
+
+#     if ans == "dino":
+#         print("correct")
+#     elif ans!="dino":
+#         print("incorrect")
+
+# q5()
+
