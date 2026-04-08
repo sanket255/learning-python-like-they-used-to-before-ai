2bcc4f2f91aec6c9c9d9a196deac77de6873f3c6
sanket255 <sanketwaghmare340@gmail.com>
2026-04-08 21:37:57 +0530

    added main function deat with error handeling wow i really leveled up in these 12 days i kinda impressed my self looking at this and realising i have not taken any external help im wow for myself

--- day-13/main.py
+++ day-13/main.py
@@ -5,38 +5,77 @@ print("Contact Manager App")
 # Add contact (name, phone, email)
 def add_contacts():
     name=input("Enter the name of the person: ")
-    number=int(input("Enter the Phone Number of the person: "))
+    while True:
+        try:
+            number=int(input("Enter the Phone Number of the person: "))
+            break
+        except ValueError:
+            print ("Enter a number please")
+            continue
+    
     email=input("Enter the E-Mail of the person (optional, leave blank if not know):")
+        
     with open("contacts.txt", "a" ) as file:
         file.write(f"{name}: {number}, {email}"+ "\n")
+    print("Contact added successfully!!!")
 
 # View all contacts
-def view_contacts():
-    with open ("contacts.txt", "r" )as file:
-        print(file.read())
 
+def view_contacts():
+    try:
+        with open ("contacts.txt", "r" )as file:
+            print(file.read())
+    except FileNotFoundError:
+        print("file not found")
 
 # Search contact
 def search_contacts():
-    keyword=input("search by name: ")
-    with open ("contacts.txt","r") as file:
-        for line in file:
-            if keyword in line:
-                print(line)
-
+    while True:
+        try:
+            keyword=input("search by name: ")
+            with open ("contacts.txt","r") as file:
+                for line in file:
+                    if keyword in line:
+                        print(line)
+                    elif keyword not in line:
+                        print("keyword not found try again")
+            break
+        except FileNotFoundError:
+            print("file not found")
+            continue
 # Delete contact
 def del_con():
     keyword1=input("search the contact by name: ")
     with open ("contacts.txt","r") as file:
         lines=file.readlines()
+    with open("contacts.txt","w") as file:   
+        for line in lines:
+            if keyword1 in line:
+                print(f"deleted line: {line}")
+            if keyword1 not in line:
+                file.write(line)
+
+def main():
+    ask=0
+    while ask!="quit":
+        ask=input("Add contact(a)/ search contact(b)/ read contact list(r)/ delete contact(d) or quit?: ")
+        if ask=="a":
+            add_contacts()
+        if ask=="r":
+            view_contacts()
+        if ask == "b":
+            search_contacts()
+        if ask == "d":
+            del_con()
+        if ask == "quit":
+            break
+
+
+
+
 
         
         
-    with open("contacts.txt","w") as file:   
-        for line in lines:
-            if keyword1 not in line:
-                file.write(line)
-                
 
 
 
