1044a5a0bcdca50310768ee9fde6ecbd3507e097
sanket255 <sanketwaghmare340@gmail.com>
2026-04-08 20:45:11 +0530

    add_contact function added

--- day-13/main.py
+++ day-13/main.py
@@ -2,6 +2,14 @@
 # Build a  (simple, but organized with functions):
 
 print("Contact Manager App")
+# Add contact (name, phone, email)
+def add_contacts():
+    name=input("Enter the name of the person: ")
+    number=int(input("Enter the Phone Number of the person: "))
+    email=input("Enter the E-Mail of the person (optional, leave blank if not know):")
+    with open("contacts.txt", "a" ) as file:
+        file.write(f"{name}: {number}, {email}"+ "\n")
+
 
 
 
