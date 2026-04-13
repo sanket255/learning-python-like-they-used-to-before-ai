04cfb1c18fb90dc534a548441b147cbe94164a4b
sanket255 <sanketwaghmare340@gmail.com>
2026-04-13 22:11:05 +0530

    day-18 im taking a break now ill complete it before going to sleep

--- day-18/main.py
+++ day-18/main.py
@@ -1,28 +1,28 @@
-# class Student:
-#     def __init__(self,name,id,grades):
-#         self.name=name
-#         self.id=id
-#         self.grades=grades
-#     def info(self):
-#         print(f"{self.name}'s id is {self.id} and got {self.grades} grade!!!")
+class Student:
+    def __init__(self,name,id,grades):
+        self.name=name
+        self.id=id
+        self.grades=grades
+    def info(self):
+        print(f"{self.name}'s id is {self.id} and got {self.grades} grade!!!")
 
 
 
 
 
-# sanket = Student("sanket",342,"b+")
+sanket = Student("sanket",342,"b+")
 
-# # print(sanket.grades)
-# sanket.info()
+# print(sanket.grades)
+sanket.info()
 
-# jon = Student("Jon",324,"A")
-# jon.info()
-
-
+jon = Student("Jon",324,"A")
+jon.info()
 
 
-# riti = Student("Riti",343,"c")
-# riti.info()
+
+
+riti = Student("Riti",343,"c")
+riti.info()
 
 
 
