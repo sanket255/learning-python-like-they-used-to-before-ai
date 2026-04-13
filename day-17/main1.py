5d87f7ad60a0454331037cd8a6d37f96ee7169db
sanket255 <sanketwaghmare340@gmail.com>
2026-04-13 22:10:17 +0530

    file 2 of day-17 cause i thought its not enough

--- day-17/main1.py
+++ day-17/main1.py
@@ -1,20 +1,20 @@
-# class People:
-#     def __init__(self,name,age,city):
-#         self.name=name
-#         self.age=age
-#         self.city=city
-#     def info(self):
-#         print(f"{self.name} is {self.age} and live in {self.city}")
+class People:
+    def __init__(self,name,age,city):
+        self.name=name
+        self.age=age
+        self.city=city
+    def info(self):
+        print(f"{self.name} is {self.age} and live in {self.city}")
 
 
-# p1=People("sanket", 19, "Mumbai")
-# p1.info()
-# p2=People("riti",18,"mumbai")
-# p2.info()
-# p3=People("jon", 23, "san F")
-# p3.info()
-# p4=People("krsna", 37, "delhi")
-# p4.info()
+p1=People("sanket", 19, "Mumbai")
+p1.info()
+p2=People("riti",18,"mumbai")
+p2.info()
+p3=People("jon", 23, "san F")
+p3.info()
+p4=People("krsna", 37, "delhi")
+p4.info()
 
 
 
