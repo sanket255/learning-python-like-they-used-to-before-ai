3f5c13062a915f2f3c5d63c825a04375f8bc8fb3
sanket255 <sanketwaghmare340@gmail.com>
2026-04-14 23:31:41 +0530

    phase-2 complete

--- day-19 /main.py
+++ day-19 /main.py
@@ -20,7 +20,28 @@ while ask!="quit":
     # pair=dict(zip(user_m,user_r))
     # print(pair)
 
+# Store in file (movie: rating format)
+    
+    with open("movie.txt", "a") as file:
+        file.write(f"{movie}: {rating}")
 
+    with open ("movie.txt","r") as file:
+        print(f"{user_m}: {user_r}")
+
+# Show average rating of all movies
+
+
+
+
+
+
+# Search for a specific movie's rating
+    keyword=input("Enter the movie name: ")
+    with open ("movie.txt", "r") as file:
+
+        for line in file:
+            if keyword in line:
+                print (line)
 
 
 
