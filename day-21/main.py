c04225b8e0aea93aa9f7f96fd3d8d7de7d61d700
sanket255 <sanketwaghmare340@gmail.com>
2026-04-17 00:30:19 +0530

    yah im just pushing this mess ill deal with it tomorrow im very sleepy ngl

--- day-21/main.py
+++ day-21/main.py
@@ -1,3 +1,5 @@
+import json
+
 # Class:
 # NoteBook
 
@@ -8,25 +10,46 @@ class NoteBook:
 # adds note to memory
 
     def add_note(self,date,note):
-        notes=[]
-        self.note=note
-        notes.append(self.note)
-# saves to file
-        with open ("notebook.txt", "a") as file:
-            file.write((f"{date}: {self.note}") + "\n")
+        try:
+            with open ("notebook.json","r") as file:
+                data=json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {}
+        
+        if date in data:
+            data[date].append(note)
+        else:
+            data[date] = [note]
+
+        with open ("notebook.json" , "w") as file:
+            json.dump(data,file, indent =4)
+            
+#         notes = {}
+#         all_notes = []
+#         self.note=note
+#         notes.(self.note)
+# # saves to file
+#         with open ("notebook.json", "w") as file:
+#             json.dump((f"{date}: {self.note}") + "\n")
 
 # show_notes()
     def show_notes(self):
-        with open ("notebook.txt", "r") as file:
-            print(file.read())
+        try:
+            with open ("notebook.json", "r") as file:
+                data=(json.load(file))
+            for date in data
+
+        except FileNotFoundError:
+            print("File doesnt exist!!!")
 
 
-notebook= NoteBook()
-ask="0"
-while ask!='quit':
-    notebook.add_note(input("Enter the date today: "),input("Enter the note Today that Tomorrow you will read: "))
-# prints all notes from file
-    notebook.show_notes()
-    ask=input("another note? ( y/quit ):")
+
+# notebook= NoteBook()
+# ask="0"
+# while ask!='quit':
+#     notebook.add_note(input("Enter the date today: "),input("Enter the note Today that Tomorrow you will read: "))
+# # prints all notes from file
+#     notebook.show_notes()
+#     ask=input("another note? ( y/quit ):")
 
 
