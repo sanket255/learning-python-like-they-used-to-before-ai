ffb2c339d313accce2583bde9140967dba45fb4c
sanket255 <sanketwaghmare340@gmail.com>
2026-04-20 00:24:29 +0530

    again borrow_book lets user borrow the book

--- day-24/mian.py
+++ day-24/mian.py
@@ -10,5 +10,13 @@ class Library:
         elif title not in self.shelf:
             (self.shelf).append(title)
             print(self.shelf)
+            
+    def borrow_book(self,title):
+        if title in self.borrow:
+            print(f"{title} this book is already borrowed")
+            print(f"{self.shelf} here are other options we have")            
+        elif title in self.shelf:
+            (self.shelf).remove(title) and (self.borrow).append(title)
+            print(self.borrow)
     
 
