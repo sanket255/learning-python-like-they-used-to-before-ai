c64e632fb6141882d8c80a14afc8d36648ba8593
sanket255 <sanketwaghmare340@gmail.com>
2026-04-20 00:23:52 +0530

    add_book method does what it says it adds book in shelf list

--- day-24/mian.py
+++ day-24/mian.py
@@ -4,6 +4,11 @@ class Library:
         self.shelf = []
         self.borrow= []
 
-
+    def add_book(self,title):
+        if title in self.shelf:
+            print(f"the book {title} already exist!!")
+        elif title not in self.shelf:
+            (self.shelf).append(title)
+            print(self.shelf)
     
 
