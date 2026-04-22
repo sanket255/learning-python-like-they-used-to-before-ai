ae00e1f262224aae48874c17501337b540f2b5be
sanket255 <sanketwaghmare340@gmail.com>
2026-04-22 21:59:35 +0530

    yah its difficult to understand man

--- day-26/main.py
+++ day-26/main.py
@@ -5,10 +5,35 @@
 # Tasks: ['X']
 
 class User:
-    print("User!")
-    def __init__(self,user,task):
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+    def add_task(self, task):
+        (self.tasks).append(task)
+    def show (self):
+        print(f"{self.name}: {self.tasks}")
 
-        self.user = user
-        self.task = []
+class TaskManager:
+    def __init__(self):
+        self.data = {}
+    def save_in_dict (self,name,task):
+        if self.data[name] in task:
+            self.data[name].append(task)
+        else:
+            self.data[name] = [task]
+    def show_dict(self , name , task):
+        print(f"{name}: {task}")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
