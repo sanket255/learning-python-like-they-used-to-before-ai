6ef7a0cf64468cc72a979c4aa11cab97fc3c9868
sanket255 <sanketwaghmare340@gmail.com>
2026-04-22 20:43:05 +0530

    today we are again leveling up as usual we have been following same single class structure for a week now today we are working on 2 classes 1 user that i have just done and task manager up coming

---
+++ day-26/main.py
@@ -0,0 +1,14 @@
+# User: Sanket
+# Tasks: ['A', 'B']
+
+# User: Riti
+# Tasks: ['X']
+
+class User:
+    print("User!")
+    def __init__(self,user,task):
+
+        self.user = user
+        self.task = []
+
+
