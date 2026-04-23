580abc9da7aad99e11ef40609b84d24f98a5ab48
sanket255 <sanketwaghmare340@gmail.com>
2026-04-23 21:01:55 +0530

    class is done writing now we go object loop

--- day-27/main.py
+++ day-27/main.py
@@ -1,2 +1,14 @@
 class Calculator:
- 
+    def add(self,a,b):
+        return a+b
+    def sub (self,a,b):
+        return a-b
+    def mul (self,a,b):
+        return a*b
+    def div(self,a,b):
+        if b == 0 :
+            return("a number cant be divided by 0")
+        else:
+            return a/b
+    
+
