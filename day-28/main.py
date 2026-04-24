603852453046bc4be6b7f31ed93ca9ea97338891
sanket255 <sanketwaghmare340@gmail.com>
2026-04-24 23:45:10 +0530

    i think im at the point where i really dont need to write these kind of commits massages but wtf its my repo, my repo my chioce

--- day-28/main.py
+++ day-28/main.py
@@ -1,3 +1,10 @@
 class ContactBook:
     def __init__(self):
         self.data = {}
+        
+    def add_name(self,name,contact):
+        if name in self.data:
+            self.data[name].append(contact)
+        else:
+            self.data[name] = contact
+        # return self.data
