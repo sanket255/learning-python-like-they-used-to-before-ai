d1fe0f1523fd65c94e03e9aa1ff5e331c6db8fb2
sanket255 <sanketwaghmare340@gmail.com>
2026-04-27 21:10:30 +0530

    it made me struggle ik its easy but see that ego problem that made me doubt my skills and ate 1 hr in this i used loop twice the True and with a condition made no sence printed stuff instead of using return made mistakes multiple time but here we are

--- day-30/main.py
+++ day-30/main.py
@@ -12,6 +12,13 @@ import random
 class Game:
     def __init__(self):
         self.secrate = random.randint( 1 , 100 ) 
+    def guess (self, number):
+        if number > self.secrate:
+            return "too high"
+        if number < self.secrate:
+            return "too low"
+        if number == self.secrate:
+            return "correct!"
 
 
 
