cff2ec13a58c8a4cd79126ad0ac1ce3413b6af4c
sanket255 <sanketwaghmare340@gmail.com>
2026-04-28 23:49:13 +0530

    object done

--- day-31/main.py
+++ day-31/main.py
@@ -28,3 +28,36 @@ class FileReader:
 
 
 
+fr = FileReader()
+
+while True:
+    filename = input("Enter the filename(Enter name of the file to read) / look into the filenames(lookin) / search / (or quit ): ")
+    if filename == "quit":
+        break
+
+    elif filename == "lookin":
+        result = fr.show_list()
+        for i, result in enumerate(result):
+            print(f"{[i]}: {result}")
+
+    elif filename == "search":
+        keyword = input("search by keyword: ")
+        fr.search( filename, keyword )
+
+    else:
+        while True:
+            try:
+                file = fr.read_files(filename)
+                print(file)
+                break
+            except FileNotFoundError:
+                print("this file does not exist in our database!")
+                continue
+    
+
+
+
+
+# List: Use os.listdir() to show the user what is there.
+# Filter: Use os.path.isfile() to ensure they only see files, not folders.
+# Search: If they type a name, use os.path.exists() to verify it before trying to open it.
