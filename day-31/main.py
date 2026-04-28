f3c1906697e2f7b8732630060acb449a54cd9e60
sanket255 <sanketwaghmare340@gmail.com>
2026-04-29 00:10:12 +0530

    wow im really impressing myself day-by-day see now im going to jinx my progress

--- day-31/main.py
+++ day-31/main.py
@@ -8,6 +8,7 @@ class FileReader:
     def read_files(self, filename):
         with open (f"{filename}.txt", "r") as file:
             return (file.read())
+   
     def show_list(self):
         folder = "/home/scar/learnpy/day-31"
         return os.listdir(folder)
@@ -31,31 +32,40 @@ class FileReader:
 fr = FileReader()
 
 while True:
-    filename = input("Enter the filename(Enter name of the file to read) / look into the filenames(lookin) / search / (or quit ): ")
+    
+    filename = input("Enter the filename(Enter name of the file to read)  / (or quit ): ")
+    
     if filename == "quit":
         break
-
-    elif filename == "lookin":
-        result = fr.show_list()
-        for i, result in enumerate(result):
-            print(f"{[i]}: {result}")
-
-    elif filename == "search":
-        keyword = input("search by keyword: ")
-        fr.search( filename, keyword )
-
+    
     else:
         while True:
-            try:
-                file = fr.read_files(filename)
-                print(file)
-                break
-            except FileNotFoundError:
-                print("this file does not exist in our database!")
-                continue
+                try:
+                    file = fr.read_files(filename)
+                    print(file)
+                    break
+                except FileNotFoundError:
+                    print("this file does not exist in our database!")
+                    continue
     
 
 
+    ask = input("look into the files we have? ( y / quit ): ")
+    if ask == "y":
+        result = fr.show_list()
+        for i, result in enumerate(result):
+            print(f"{i}: {result - 1}")
+
+    ask = input("do you want to search through file? ( y / quit ): ")
+    if ask == "y":
+        keyword = input("search by keyword: ")
+        search = fr.search( filename, keyword )
+        print(search)
+    
+    elif ask == "quit":
+        break
+
+        
 
 
 # List: Use os.listdir() to show the user what is there.
