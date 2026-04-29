c4ea7e64f7c0100443b73ef3a8496c986419821f
sanket255 <sanketwaghmare340@gmail.com>
2026-04-29 22:48:35 +0530

    added search feature it searches through  single file only that user chose

--- day-32/main.py
+++ day-32/main.py
@@ -5,7 +5,17 @@ class FileSearch:
         folder = "/home/scar/learnpy/day-32"
         return os.listdir(folder)
 
- 
+    def search_infile(self,filename,keyword):
+        match= []
+        with open (f"{filename}", "r") as file:
+            for lines in file:
+                
+                if keyword.lower() in lines.lower():
+                    match.append(lines.strip())
+
+            return match
+    
+
 
 
 
