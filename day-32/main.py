4cf4cfb44f4f895b7f2736c8097aeb0f4a8763e5
sanket255 <sanketwaghmare340@gmail.com>
2026-04-29 23:54:07 +0530

    object is done i iterated this for a while to learn how to use it propperly i made the experimetnal mistake yester that i fixed today

--- day-32/main.py
+++ day-32/main.py
@@ -23,12 +23,34 @@ class FileSearch:
     
         for f in files:
             if f.endswith(".txt"):
-                result = self.search_infile(f,keyword)
+                full_path = os.path.join(folder,f)
+                result = self.search_infile(full_path,keyword)
                 final_result[f] = result
     
         return final_result
 
 fs = FileSearch()
+
+print("INSTRUCTIONS!!\n\n" )
+print("1. Enter the file extention when entering the file name \n\n example(file.txt)\n\n\n")
+
+
+while True:
+    show = fs.get_list()
+    print(show)
+
+    ask = input("do you want to search through a perticuler file(a) \n\nor in everyfile in the folder(b) or (quit): ")
+    if ask == "quit":
+        break
+    if ask == "a":
+        filename = input("name the file you want to search through: ")
+        keyword = input("Enter the word that you want to search  through: ")
+        result = fs.search_infile(filename, keyword)
+        print (result)
+    if ask == "b":
+        keyword = input("Enter the word that you want to search  through: ")
+        result = fs.search_infolder(keyword)
+        print (result)
 
 
 
